312ab782ee6831e038e6367e1b9d7d01e84a6967
PedroRCSilva <pedro.rsilva@sptech.school>
2026-02-23 14:57:27 -0300

    initial structure

---
+++ src/test/java/com/pedro/azure_ad_sso_implementation/AzureAdSsoImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.azure_ad_sso_implementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureAdSsoImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
